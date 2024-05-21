14825e469108cfca6c79d118127184f49a2d5129
KhanzaidGithub <zaidscestudent@gmail.com>
2024-05-21 19:34:55 +0500

    Almost Completeted

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-let productstitle = document.querySelectorAll('.product-title');
-let searchbtn = document.getElementById('search-btn');
+// let productstitle = document.querySelectorAll('.product-title');
+// let searchbtn = document.getElementById('search-btn');
 
-searchbtn.addEventListener('click', () => {
+// searchbtn.addEventListener('click', () => {
+//     let input = document.getElementById('input').value.toLowerCase();
+//     let found = false;
+
+
+//     searchbtn.addEventListener('click', () => {
+//         let input = document.getElementById('input').value.toLowerCase();
+//         let found = false;
+//         if(input.trim()!==''){
+//             productstitle.forEach(element => {
+//                 if(element.textContent.toLowerCase().includes(input)){
+//                     found=true;
+//                 }
+//             });
+//             if(found){
+//                 alert('product title matches');
+//             }
+//             else{
+//                 alert('product title does not match');
+//             }
+//         }
+//         else{
+//             alert('Please enter txt in the input')
+//         }
+//     });
+// });
+
+function search() {
     let input = document.getElementById('input').value.toLowerCase();
-    let found = false;
+    let products = document.querySelectorAll('.product');
 
+    products.forEach((product) => {
+        let title = product.querySelector('#product-title');
 
-    searchbtn.addEventListener('click', () => {
-        let input = document.getElementById('input').value.toLowerCase();
-        let found = false;
-        if(input.trim()!==''){
-            productstitle.forEach(element => {
-                if(element.textContent.toLowerCase().includes(input)){
-                    found=true;
-                }
-            });
-            if(found){
-                alert('product title matches');
-            }
-            else{
-                alert('product title does not match');
-            }
-        }
-        else{
-            alert('Please enter txt in the input')
+        let value = title.innerHTML || title.innerText || title.textContent;
+
+        if (value.toLowerCase().indexOf(input) > -1) {
+            product.style.display = '';
+        } else {
+            product.style.display = 'none';
         }
     });
-});
+}
 
